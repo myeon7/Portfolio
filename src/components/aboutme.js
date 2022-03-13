b1a8d92e9f19df235a2569518c35c766d619cb31
Matthew Yeon <m.jiwooyeon@gmail.com>
2022-03-13 17:49:25 -0400

    included portfolio, MYTube Project (incomplete)

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import pi1 from '../media/profileimg/Profile-01.jpg';
+import pi2 from '../media/profileimg/Profile-02.jpg';
+import pi3 from '../media/profileimg/Profile-03.jpg';
+import pi4 from '../media/profileimg/Profile-04.jpg';
+import pi5 from '../media/profileimg/Profile-05.jpg';
+import pi6 from '../media/profileimg/Profile-06.jpg';
 
 function Aboutme({scrollToAboutme}){
+
+    const [profile, setProfile] = useState({
+        currentImage: 0,
+        images: [{
+            profile_img1: pi1,
+            profile_img2: pi2,
+            profile_img3: pi3,
+            profile_img4: pi4,
+            profile_img5: pi5,
+            profile_img6: pi6,
+        }]
+    });
+
+    console.log(profile);
+
     return(
         <div id="c1" className="section clearfix" ref={scrollToAboutme}>
             <div id="links">
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,19 +1,26 @@
 import React, {useState, useRef} from 'react';
 import Project from './project';
 // Project Covers
+import portfolio_cover from '../media/projects/portfolio_cover.jpg';
+import mytube_cover from '../media/projects/mytube_cover.jpg';
 import chocoblock_cover from '../media/projects/chocoblock_cover.jpg';
 import lululemon_cover from '../media/projects/lululemon_cover.jpg';
 import spotifyrec_cover from '../media/projects/spotifyrec_cover.jpg';
 import design_cover from '../media/projects/design_cover.jpg';
-// Project1: Lululemon
+
+// Project: Lululemon
+import portfolio_img1 from '../media/projects/portfolio_img1-1.jpg';
+import portfolio_img2 from '../media/projects/portfolio_img2-1.jpg';
+import portfolio_img3 from '../media/projects/portfolio_img3-1.jpg';
+// Project: Lululemon
 import lulu_original from '../media/projects/lululemon_original.jpg';
 import lulu_main from '../media/projects/lululemon_main.jpg';
 import lulu_benchmark from '../media/projects/lululemon_benchmark.jpg';
 import lulu_highprototype from '../media/projects/lululemon_highprototype.jpg';
 import lulu_learningprogress from '../media/projects/lululemon_learningprogress.jpg';
-// Project2: Chocoblock
+// Project: Chocoblock
 import chocoblock from '../media/projects/chocoblock_content.jpg';
-// Project3: Design
+// Project: Design
 import design_img1 from '../media/projects/design_img2.jpg';
 
 
@@ -21,6 +28,45 @@ function Projects({scrollToProjects}){
 
     const idx = useRef(0);
     const [projects, setProject] = useState([
+        {
+            title: "Welcome to my portfolio (m.jiwooyeon.com)",
+            cover: portfolio_cover,
+            description: "Created with React",
+            // time: "Oct 2021 - Dec 2021",
+            // content
+            // task: "Analyze ",
+            role: "",
+            text1: "Hello!",
+            image1: portfolio_img1,
+            image2: portfolio_img2,
+            image3: portfolio_img3,
+        },
+        {
+            title: "YouTube Clone",
+            cover: mytube_cover,
+            type: "(Personal Project)",
+            prototype: "",
+            description: "React & YouTube API",
+            time: "Jul 2021 - Aug 2021",
+            // content
+            task: "Cloning YouTube to learn how to develop a project using external API data",
+            role: "Front-End Developer",
+            final_header1: "Future Plan",
+            final_text1: "Finally, ",
+        },
+        {
+            title: "Spotify Song Recommendation",
+            cover: spotifyrec_cover,
+            type: "(Group Project)",
+            description: "Python & Spotify API",
+            time: "Oct 2021 - Dec 2021",
+            // content
+            task: "Analyze ",
+            role: "",
+            text1: "Analyze the correlation between song features and its popularity and give recommendation of songs through machine learning using Python",
+            final_header1: "Future Plan",
+            final_text1: "Finally, ",
+        },
         {
             title: "Lululemon Renewal",
             cover: lululemon_cover,
@@ -37,39 +83,14 @@ function Projects({scrollToProjects}){
             text1: "I re-designed the website using the original contents from lululemon offical webpage. I used basic HTML and CSS to develop frontend part of the page and applied JavaScript (mainly JQuery) to include dynamics and animations to improve the user interface.",
             image1: lulu_benchmark,
             text2: "To incorporate and apply modern features to the renewed lululemon website, I looked over various web templates from pinterest and visited 1000+ websites from awwwards, CSS Design Awards, and Web Design File. I analyzed common content layout / design / motion component throughout various web pages and benchmarked them based on features that best matches with user experience in the original lululemon website.",
-            image2: lulu_highprototype,
-            text3: "Through incorporation of different aspects from benchmarked web templates, I used Figma to complete high-fidelity prototype of re-designed lululemon website.",
-            image3: lulu_learningprogress,
-            text4: "TEXT",
+            image2: lulu_learningprogress,
+            text3: "For the features that I need assitance with, I had to personally inspect the code from the benchmarked website to understand it's function and adjust to my coding.",
+            image3: lulu_highprototype,
+            text4: "Through incorporation of different aspects from benchmarked web templates, I used Figma to complete high-fidelity prototype of re-designed lululemon website.",
             final_header1: "Challenge",
-            final_text1: "TEXT",
+            final_text1: "Difficulty of incorporating more dynamic features into the website. Limited Coding skills to implement more dynamic features. Designing based on the purpose of website.",
             final_header2: "Future Plan",
-            final_text2: "As I researched for modern web design trend, I was amazed to see the endless posibilities of design aspect of web features and functionalities using JavaScript.",
-        },
-        {
-            title: "Spotify Song Recommendation",
-            cover: spotifyrec_cover,
-            type: "(Group Project)",
-            description: "Python & Spotify API",
-            time: "Oct 2021 - Dec 2021",
-            // content
-            task: "Analyze ",
-            role: "",
-            text1: "Analyze the correlation between song features and its popularity and give recommendation of songs through machine learning using Python",
-            final_header: "Future Plan",
-            final_text: "Finally, ",
-        },
-        {
-            title: "YouTube Clone",
-            type: "(Personal Project)",
-            prototype: "",
-            description: "React & YouTube API",
-            time: "Jul 2021 - Aug 2021",
-            // content
-            task: "Cloning YouTube to learn how to develop a project using external API data",
-            role: "Front-End Developer",
-            final_header: "Future Plan",
-            final_text: "Finally, ",
+            final_text2: "As I researched for modern web design trend, I was amazed to see the endless posibilities of design aspect of web features and functionalities using JavaScript. Learn more indepth of webGL to implement more dynamic features to the page. This was a static website, but will try creating a dynamic website using external APIs.",
         },
         {
             title: "Choco Block",
@@ -83,12 +104,12 @@ function Projects({scrollToProjects}){
             role: "UX Brand + Product Designer",
             image1: chocoblock,
         },
-        {
-            title: "Next Project",
-            type: "(N.A.)",
-            description: "In Progress..",
-            // content
-        },
+        // {
+        //     title: "Next Project",
+        //     type: "(N.A.)",
+        //     description: "In Progress..",
+        //     // content
+        // },
         {
             title: "Design Work",
             cover: design_cover,
